81c842c591ddf99ef45398700d1f5b9fd0a69f44
Iulian Carnaru <iulian.carnaru@vodafone.com>
2021-02-02 12:22:40 +0000

    added dummy data and accordion component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import Accordion from './Accordion';
+
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a front end javascript library.',
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is one of the best libraries.',
+  },
+  {
+    title: 'How do you use React?',
+    content: 'You use React by creating reusable components.',
+  },
+];
 
 const App = () => {
   return (
     <div>
-      <p>APP</p>
+      <Accordion items={items} />
     </div>
   );
 };
