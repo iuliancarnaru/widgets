f71bcf52c8eed62858d735efee2142e87dc8a428
iuliancarnaru <iulian.carnaru@gmail.com>
2021-02-02 13:00:42 +0000

    adding active logic

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,28 +1,28 @@
 import React, { useState } from 'react';
 
 const Accordion = ({ items }) => {
-  const [activeIndex, setActiveIndex] = useState(0);
+  const [activeIndex, setActiveIndex] = useState(null);
 
   const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? 'active' : '';
+
     return (
       <React.Fragment key={item.title}>
-        <div className="title active" onClick={() => setActiveIndex(index)}>
+        <div
+          className={`title ${active}`}
+          onClick={() => setActiveIndex(index)}
+        >
           <i className="dropdown icon"></i>
           {item.title}
         </div>
-        <div className="content active">
+        <div className={`content ${active}`}>
           <p>{item.content}</p>
         </div>
       </React.Fragment>
     );
   });
 
-  return (
-    <div className="ui styled accordion">
-      {renderedItems}
-      <h1>{activeIndex}</h1>
-    </div>
-  );
+  return <div className="ui styled accordion">{renderedItems}</div>;
 };
 
 export default Accordion;
