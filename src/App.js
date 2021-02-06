598a783cee3767996b22dddaaa9cbb61c843cf8d
iuliancarnaru <iulian.carnaru@gmail.com>
2021-02-06 11:39:12 +0000

    implementing basic routing v2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,70 @@
-import React from 'react';
-// import Accordion from './components/Accordion';
-// import Search from './components/Search';
-// import Dropdown from './components/Dropdown';
+import React, { useState } from 'react';
+import Accordion from './components/Accordion';
+import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
+import Route from './components/Route';
+
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a front end javascript library.',
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is one of the best libraries.',
+  },
+  {
+    title: 'How do you use React?',
+    content: 'You use React by creating reusable components.',
+  },
+];
+
+const options = [
+  {
+    label: 'Red',
+    value: 'red',
+  },
+  {
+    label: 'Green',
+    value: 'green',
+  },
+  {
+    label: 'Blue',
+    value: 'blue',
+  },
+  {
+    label: 'Yellow',
+    value: 'yellow',
+  },
+];
+
+// const showComponent = (route, component) => {
+//   return window.location.pathname === route ? component : null;
+// };
 
 const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div>
-      <Translate />
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          labelText="Select a color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
     </div>
   );
 };
